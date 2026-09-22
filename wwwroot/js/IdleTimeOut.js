1d0e05fa52251be63be496e9a18e1b45be72c418
gulievmt <gulievmt@rambler.ru>
2026-09-22 17:59:28 +0400

    IdleTimeOut

---
+++ wwwroot/js/IdleTimeOut.js
@@ -0,0 +1,27 @@
+﻿let idleTimer = null;
+let dotnetRef = null;
+let timeoutMs = null;
+
+const activityEvents = ["mousemove", "mousedown", "keydown", "scroll", "touchstart"];
+
+export function start(dotnetHelper, timeoutInMs) {
+    dotnetRef = dotnetHelper;
+    timeoutMs = timeoutInMs;
+
+    resetTimer();
+    activityEvents.forEach(evt => document.addEventListener(evt, resetTimer, true));
+}
+
+function resetTimer() {
+    clearTimeout(idleTimer);
+    idleTimer = setTimeout(() => {
+        console.log("Idle timeout reached. Invoking .NET method.");
+        dotnetRef?.invokeMethodAsync("OnIdleTimeout");
+    }, timeoutMs);
+}
+
+export function stop() {
+    clearTimeout(idleTimer);
+    activityEvents.forEach(evt => document.removeEventListener(evt, resetTimer, true));
+    dotnetRef = null;
+}
